420a52fd40b306ad4b77c85ac66d4bfc94f2e1c5
ismail <ismail.manared@gmail.com>
2018-10-31 11:55:18 +0100

    min sol

--- examples/d_auto/d_auto.py
+++ examples/d_auto/d_auto.py
@@ -21,18 +21,30 @@ def solve():
     problem.addVariable("X50", range(nb50))
     problem.addVariable("X20", range(nb20))
     problem.addVariable("X10", range(nb10))
+    problem.addVariable("X", range(nb10))
     
-    problem.addConstraint(lambda x1 , x2 , x50 , x10 , x20 : 100 * ( total - price ) == x1 * 100 + x2 * 200 + x20 * 20 + x10 * 10 + x50 *50, ["X1","X2","X50","X20","X10"])
+    problem.addConstraint(lambda x1 , x2 , x50 , x10 , x20 :100 * ( total - price ) == x1 * 100 + x2 * 200 + x20 * 20 + x10 * 10 + x50 *50, ["X1","X2","X50","X20","X10"])
+    problem.addConstraint(lambda x , x1 , x2 , x50 , x10 , x20 : x == x1 + x2 + x20 + x10 + x50, ["X","X1","X2","X50","X20","X10"])
     
     # Get the solutions.
     solutions = problem.getSolutions()
-    return solutions
+    minX = solutions[0]["X"]
+    solution = solutions[0]
+    
+    for s in solutions :
+        if s["X"] < minX:
+            minX = s["X"]
+            solution = s
+        
+    return [solution]
 
 
 def main():
     solutions = solve()
     # Print the solutions
     print("Found %d solution(s)!" % len(solutions))
+    
+    
 
 if __name__ == "__main__":
     main()
