9112038c4fa7cb0f0983137da29fbdadaaffb681
ismail <ismail.manared@gmail.com>
2018-10-31 11:32:18 +0100

    add files

---
+++ examples/d_auto/d_auto.py
@@ -0,0 +1,38 @@
+#
+# Sudoku puzzle solver by by Luigi Poderico (www.poderico.it).
+#
+import sys
+from constraint import Problem
+
+
+nb2 = 12
+nb1 = 12
+nb50 = 90
+nb20 = 56
+nb10 = 21
+
+price = 15.50
+total = 20
+ 
+def solve():
+    problem = Problem()
+    problem.addVariable("X1", range(nb1))
+    problem.addVariable("X2", range(nb2))
+    problem.addVariable("X50", range(nb50))
+    problem.addVariable("X20", range(nb20))
+    problem.addVariable("X10", range(nb10))
+    
+    problem.addConstraint(lambda x1 , x2 , x50 , x10 , x20 : 100 * ( total - price ) == x1 * 100 + x2 * 200 + x20 * 20 + x10 * 10 + x50 *50, ["X1","X2","X50","X20","X10"])
+    
+    # Get the solutions.
+    solutions = problem.getSolutions()
+    return solutions
+
+
+def main():
+    solutions = solve()
+    # Print the solutions
+    print("Found %d solution(s)!" % len(solutions))
+
+if __name__ == "__main__":
+    main()
